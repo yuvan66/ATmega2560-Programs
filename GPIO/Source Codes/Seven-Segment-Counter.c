82ff91feff1f092b87278686b0cc774694f87f47
yuvan66 <yuvanudt@gmail.com>
2026-01-31 16:04:47 +0530

    Fixed major electrical issue

--- GPIO/Source Codes/Seven-Segment-Counter.c
+++ GPIO/Source Codes/Seven-Segment-Counter.c
@@ -15,12 +15,20 @@ int main()
         long int a = 0;
         while(a<40000)
         {
-          portk_set(0b00000001);
-          portk_out(0x01);
-          disp_SS(i);
-          portk_set(0b00000010);
-          portk_out(0x02);
-          disp_SS(j);
+          // portk_set(0b00000001);    --This commented set of lines had a major electrical issue causing Direction Register 
+          // portk_out(0x01);          to change at very high speed, leading to malfunctioning of the Register.
+          // disp_SS(i);               So, these are changed as below to set the Direction Register 
+          // portk_set(0b00000010);    only once at the start of the program.--
+          // portk_out(0x02);
+          // disp_SS(j);
+
+          // Updated lines
+          disp_SS(i);                  //Value for first digit               
+          portk_out(0x01);             //Activate first digit
+          portk_out(0x03);             //Turn off both digits
+          disp_SS(j);                  //Value for second digit
+          portk_out(0x02);             //Activate second digit
+          portk_out(0x03);             //Turn off both digits
           a++;
         }
       }
@@ -30,6 +38,8 @@ int main()
     
   }  
 }
+
+
 // for (int i=0;i<10;i++)
     // {
     //   for (int j=0;j<10;j++)

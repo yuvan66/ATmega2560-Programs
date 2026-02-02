04709c836d1d1a66966532691e2ae1151554ed6c
yuvan66 <yuvanudt@gmail.com>
2026-02-02 21:22:16 +0530

    Added Functions for other ports in the header

--- GPIO/Headers/latest.h
+++ GPIO/Headers/latest.h
@@ -1,8 +1,71 @@
+#define porta_dir (*(volatile uint8_t *)0x21)
+#define porta_dat (*(volatile uint8_t *)0x22)
+#define portb_dir (*(volatile uint8_t *)0x24)
+#define portb_dat (*(volatile uint8_t *)0x25)
+#define portc_dir (*(volatile uint8_t *)0x27)
+#define portc_dat (*(volatile uint8_t *)0x28)
 #define portf_dir (*(volatile uint8_t *)0x30)
 #define portf_dat (*(volatile uint8_t *)0x31)
 #define portk_dir (*(volatile uint8_t *)0x107)
 #define portk_dat (*(volatile uint8_t *)0x108)
 //#define portk_rd (*(volatile uint8_t *)0x106)
+
+
+// PORTA __--__ Functions __--__ DIR,DAT,RD
+void porta_set(uint8_t adir) //set direction to PORTA
+{
+  porta_dir = adir;
+}
+
+void porta_out(volatile uint8_t adat) //ouput data for PORTA
+{
+  porta_dat = adat;
+}
+
+uint8_t porta_r() //func. to return PORTA input data
+{
+  volatile uint8_t *porta_rd;
+  porta_rd = 0x20;
+  return (*porta_rd);
+}
+
+
+// PORTB __--__ Functions __--__ DIR,DAT,RD
+void portb_set(uint8_t bdir) //set direction to PORTB
+{
+  portb_dir = bdir;
+}
+
+void portb_out(volatile uint8_t bdat) //ouput data for PORTB
+{
+  portb_dat = bdat;
+}
+
+uint8_t portb_r() //func. to return PORTB input data
+{
+  volatile uint8_t *portb_rd;
+  portb_rd = 0x20;
+  return (*portb_rd);
+}
+
+
+// PORTC __--__ Functions __--__ DIR,DAT,RD
+void portc_set(uint8_t cdir) //set direction to PORTC
+{
+  portc_dir = cdir;
+}
+
+void portc_out(volatile uint8_t cdat) //ouput data for PORTC
+{
+  portc_dat = cdat;
+}
+
+uint8_t portc_r() //func. to return PORTC input data
+{
+  volatile uint8_t *portc_rd;
+  portc_rd = 0x26;
+  return (*portc_rd);
+}
 
 
 // PORTF __--__ Functions __--__ DIR,DAT,Seven-Segment
@@ -16,11 +79,11 @@ void portf_out(volatile uint8_t fdat) //ouput data for PORTF
   portf_dat = fdat;
 }
 
-void disp_SS (volatile uint8_t x)
+uint8_t portf_r() //func. to return PORTK input data
 {
-  volatile uint8_t a[10] = {0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f};
-  portf_dat = a[x];
-  //return a[x];
+  volatile uint8_t *portf_rd;
+  portf_rd = 0x2F;
+  return (*portf_rd);
 }
 
 
@@ -35,7 +98,7 @@ void portk_out(volatile uint8_t kdat) //ouput data for PORTK
   portk_dat = kdat;
 }
 
-uint8_t portk_rd() //func. to return PORTK input data
+uint8_t portk_r() //func. to return PORTK input data
 {
   volatile uint8_t *portk_rd;
   portk_rd = 0x106;
@@ -71,7 +134,15 @@ void r2l() // led chaser right-to-left for PORTF
     }
 }
 
-void count(double x) // two-digit counter on Seven-Segment Display (00 to 99)
+
+void disp_SS (volatile uint8_t x)
+{
+  volatile uint8_t a[10] = {0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f};
+  portb_dat = a[x];
+  //return a[x];
+}
+
+void count(double x)
 {
   for (int i=0;i<10;i++)
     {
@@ -80,20 +151,12 @@ void count(double x) // two-digit counter on Seven-Segment Display (00 to 99)
         long int a = 0;
         while(a<(x*10000))
         {
-          // portk_set(0b00000001);    --This commented set of lines had a major electrical issue causing Direction Register 
-          // portk_out(0x01);          to change at very high speed, leading to malfunctioning of the Register.
-          // disp_SS(i);               So, these are changed as below to set the Direction Register 
-          // portk_set(0b00000010);    only once at the start of the program.--
-          // portk_out(0x02);
-          // disp_SS(j);
-
-          // Updated lines
-          disp_SS(i);                  //Value for first digit               
-          portk_out(0x01);             //Activate first digit
-          portk_out(0x03);             //Turn off both digits
-          disp_SS(j);                  //Value for second digit
-          portk_out(0x02);             //Activate second digit
-          portk_out(0x03);             //Turn off both digits
+          portk_set(0b00000001);
+          portk_out(0x01);
+          disp_SS(i);
+          portk_set(0b00000010);
+          portk_out(0x02);
+          disp_SS(j);
           a++;
         }
       }
